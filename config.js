ffbccf8d6fa5b8bb50b2a5710fa5bdd930ae113f
Dark NP <156697691+NP9384@users.noreply.github.com>
2024-08-28 20:33:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID ||"6ExGQBaC#g_YEyjFWWsFubfyE__jBtq2ZIyYvRbtHecQnWMSXJ4o",
+SESSION_ID: process.env.SESSION_ID ||"SE5E0aAR#nxGOTUJkrOb1fqH_blR142catRfS6xHSeg73-3JmlFE",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/6BrBx8B/Screenshot-20240313-205627-Gallery.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello IAM ALIVE WELL🟢❤️‍🔥👋 ● ♤Created by Nipuna promod♤",
 };
